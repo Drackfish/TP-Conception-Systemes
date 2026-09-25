0dea2fb255c9a06eccec12ab9ebe30b8529b01ec
Drackfish <tommonteiroalves@gmail.com>
2026-09-25 14:43:01 +0200

    sql update + ajout filtre note exacte et note max

--- Contact/src/main/java/com/example/Contact/dao/ContactRepository.java
+++ Contact/src/main/java/com/example/Contact/dao/ContactRepository.java
@@ -1,7 +1,15 @@
 package com.example.Contact.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import com.example.Contact.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContactRepository extends JpaRepository<Contact,Long> {
-    
+    //Recherche par note
+    List<Contact> findByNote(int note);
+
+    //Recherche de contact avec note max
+    List<Contact> findByNoteLessThanEqual(int note);
+
+    //Recherche par nom
+    List<Contact> findByNom(String nom);
 }
