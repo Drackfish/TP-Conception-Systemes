8a94aa3136fdb11ed20c815419de78e514fe7779
Drackfish <tommonteiroalves@gmail.com>
2026-09-25 12:54:40 +0200

    ajout boutons pour basculer entre les différentes pages

--- Contact/src/main/java/com/example/Contact/dao/ContactRepository.java
+++ Contact/src/main/java/com/example/Contact/dao/ContactRepository.java
@@ -1,6 +1,5 @@
 package com.example.Contact.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Contact.entities.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact,Long> {
